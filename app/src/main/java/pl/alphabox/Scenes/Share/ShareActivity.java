8021c14f9120501437ac62adb32c4e5370ff4a02
Robert Ogiba <ogiback@gmail.com>
2017-10-13 21:22:01 +0200

    Added OnItemClickListener to users ListView

--- app/src/main/java/pl/alphabox/Scenes/Share/ShareActivity.java
+++ app/src/main/java/pl/alphabox/Scenes/Share/ShareActivity.java
@@ -1,6 +1,9 @@
 package pl.alphabox.Scenes.Share;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -11,7 +14,8 @@ import pl.alphabox.Models.User;
 import pl.alphabox.R;
 import pl.alphabox.Utils.BaseToolbarActivity;
 
-public class ShareActivity extends BaseToolbarActivity implements IShareView {
+public class ShareActivity extends BaseToolbarActivity
+        implements IShareView, ListView.OnItemClickListener {
     @BindView(R.id.iv_app_icon)
     protected ImageView appIconView;
     @BindView(R.id.tv_app_name)
@@ -60,8 +64,14 @@ public class ShareActivity extends BaseToolbarActivity implements IShareView {
         this.adapter.addItem(user);
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.d("ShareActivity", "Selected: " + position);
+    }
+
     private void setupAdapter() {
         this.adapter = new ShareUsersAdapter(this);
         usersListView.setAdapter(adapter);
+        usersListView.setOnItemClickListener(this);
     }
 }
