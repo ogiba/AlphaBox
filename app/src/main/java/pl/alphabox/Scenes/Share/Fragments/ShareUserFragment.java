e8d25a0748e1158672323ca8650ded54d9e952e4
Robert Ogiba <ogiback@gmail.com>
2017-10-22 17:17:20 +0200

    [ShareUserFragment] Started creating base of new fragment and presenter

---
+++ app/src/main/java/pl/alphabox/Scenes/Share/Fragments/ShareUserFragment.java
@@ -0,0 +1,40 @@
+package pl.alphabox.Scenes.Share.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by robertogiba on 22.10.2017.
+ */
+
+public class ShareUserFragment extends Fragment implements IShareUserView {
+
+    public static ShareUserFragment newInstance(Bundle args) {
+        ShareUserFragment fragment = new ShareUserFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    public static class Builder {
+        public ShareUserFragment build() {
+            Bundle args = new Bundle();
+
+            return newInstance(args);
+        }
+    }
+}
---
+++ app/src/main/java/pl/alphabox/Scenes/Share/Fragments/ShareUserPresenter.java
@@ -0,0 +1,8 @@
+package pl.alphabox.Scenes.Share.Fragments;
+
+/**
+ * Created by robertogiba on 22.10.2017.
+ */
+
+public class ShareUserPresenter implements IShareUserPresenter {
+}
