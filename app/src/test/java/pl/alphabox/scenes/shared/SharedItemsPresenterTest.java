c778018cbf27606d0d446252095919001da25035
Robert Ogiba <ogiback@gmail.com>
2018-01-12 21:20:11 +0100

    Add dependecy for Mockito. Create simple test for SharedItemsPresenter class

---
+++ app/src/test/java/pl/alphabox/scenes/shared/SharedItemsPresenterTest.java
@@ -0,0 +1,35 @@
+package pl.alphabox.scenes.shared;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by robertogiba on 12.01.2018.
+ */
+
+public class SharedItemsPresenterTest {
+
+    @Mock
+    private ISharedItemsView sharedItemsView;
+
+    @InjectMocks
+    private SharedItemsPresenter sharedItemsPresenter;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void loadItems_succeed() {
+        sharedItemsPresenter.loadSharedItems();
+
+        verify(sharedItemsView).onItemsLoad(any());
+    }
+}
