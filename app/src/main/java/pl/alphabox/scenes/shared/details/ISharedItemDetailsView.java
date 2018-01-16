903104d02a83e2eb833c61d524fee5dd0ce1c18f
Robert Ogiba <ogiback@gmail.com>
2018-01-16 19:55:07 +0100

    Implement loading user data from db based on userId stored in userFile model

--- app/src/main/java/pl/alphabox/scenes/shared/details/ISharedItemDetailsView.java
+++ app/src/main/java/pl/alphabox/scenes/shared/details/ISharedItemDetailsView.java
@@ -1,11 +1,9 @@
 package pl.alphabox.scenes.shared.details;
 
-import pl.alphabox.models.UserFile;
-
 /**
  * Created by robertogiba on 16.01.2018.
  */
 
 public interface ISharedItemDetailsView {
-    void onDataResoled(String appName, String appSize, String sharedByUser, String sharingTime);
+    void onDataResoled(String appName, String appSize, String userName, String shareTime);
 }
--- app/src/main/java/pl/alphabox/scenes/shared/details/SharedItemDetailsPresenter.java
+++ app/src/main/java/pl/alphabox/scenes/shared/details/SharedItemDetailsPresenter.java
@@ -1,12 +1,20 @@
 package pl.alphabox.scenes.shared.details;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import pl.alphabox.models.User;
 import pl.alphabox.models.UserFile;
 
 /**
  * Created by robertogiba on 16.01.2018.
  */
 
-public class SharedItemDetailsPresenter implements ISharedItemDetailsPresenter {
+public class SharedItemDetailsPresenter
+        implements ISharedItemDetailsPresenter, ValueEventListener {
     private final ISharedItemDetailsView itemView;
 
     private UserFile userFile;
@@ -23,7 +31,32 @@ public class SharedItemDetailsPresenter implements ISharedItemDetailsPresenter {
 
         this.userFile = userFile;
 
-        itemView.onDataResoled(userFile.appName, "" + userFile.apkSize, userFile.userId,
-                "" + userFile.shareTime);
+        retrieveUserEmail();
+    }
+
+    private void retrieveUserEmail() {
+        if (userFile.sharedByUser == null || userFile.sharedByUser.isEmpty()) {
+            return;
+        }
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("users");
+        databaseReference.child(userFile.sharedByUser).addValueEventListener(this);
+    }
+
+    @Override
+    public void onDataChange(DataSnapshot dataSnapshot) {
+        if (dataSnapshot.exists()) {
+            User user = dataSnapshot.getValue(User.class);
+
+            if (user != null) {
+                itemView.onDataResoled(userFile.appName, "" + userFile.apkSize, user.email,
+                        "" + userFile.shareTime);
+            }
+        }
+    }
+
+    @Override
+    public void onCancelled(DatabaseError databaseError) {
+
     }
 }
