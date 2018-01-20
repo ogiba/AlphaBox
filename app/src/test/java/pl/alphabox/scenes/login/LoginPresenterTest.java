8526160c9206c899b7e8d25b2a70b47409b331bb
danielblokus <daniel.blokus@gmail.com>
2018-01-20 20:12:36 +0100

    Cleanup imports in LoginPresenterTest

--- app/src/test/java/pl/alphabox/scenes/login/LoginPresenterTest.java
+++ app/src/test/java/pl/alphabox/scenes/login/LoginPresenterTest.java
@@ -1,10 +1,5 @@
 package pl.alphabox.scenes.login;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -12,7 +7,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
@@ -20,11 +14,6 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.modules.junit4.PowerMockRunnerDelegate;
-
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyObject;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 /**
@@ -47,7 +36,7 @@ public class LoginPresenterTest {
     private DatabaseReference mockedDatabaseReference;
 
     @Before
-    public void setup() throws Exception {
+    public void setup() {
         MockitoAnnotations.initMocks(this);
 
         mockedDatabaseReference = Mockito.mock(DatabaseReference.class);
